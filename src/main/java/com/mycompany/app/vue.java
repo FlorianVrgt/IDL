8b636225cf300f17556a18e344485910fc3ac528
Vergriete Florian <vergriete@b10p16.fil.univ-lille1.fr>
2019-01-14 17:28:48 +0100

    grid

--- src/main/java/com/mycompany/app/vue.java
+++ src/main/java/com/mycompany/app/vue.java
@@ -1,11 +1,30 @@
 package com.mycompany.app;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
 public class vue {
 
+	public void dessin (int width,int height, int billeX, int billeY) {
+		int NombreCase = width * height + 2;
+		JFrame t = new JFrame();
+		JPanel pan = new JPanel (new GridLayout (10,10));
+		Border blackline = BorderFactory.createLineBorder(Color.black,1); 
+		for(int i = 0; i<NombreCase;i++){
+		   JPanel ptest = new JPanel();
+		   ptest.setBorder(blackline);
+		   pan.add(ptest);
+		}
+		pan.setBorder(blackline);
+		t.add(pan);
+		t.setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-
 	}
 
 }
