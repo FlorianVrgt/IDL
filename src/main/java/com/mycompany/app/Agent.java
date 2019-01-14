0af6a5a8482f06b6d4c37af04bf634552ac9d357
Vergriete Florian <vergriete@b10p16.fil.univ-lille1.fr>
2019-01-14 16:41:10 +0100

    init

---
+++ src/main/java/com/mycompany/app/Agent.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+public class Agent {
+	private int posX;
+	private int posY;
+	private int pasX;
+	private int pasY;
+	
+	/*
+	 * fonction pour donner la main à l'agent
+	 * change de position et de pas.
+	 */
+	public void decide() {
+		
+	}
+
+}
---
+++ src/main/java/com/mycompany/app/Environement.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import java.util.List;
+
+public class Environement {
+	List<Agent> listeAgent;
+	
+	Agent grille[][];
+	SMA sma;
+	public Environement(int nbCellulex,int nbCelluleY, int nbAgent) {
+		
+		
+		sma = new SMA()	
+	}
+
+}
