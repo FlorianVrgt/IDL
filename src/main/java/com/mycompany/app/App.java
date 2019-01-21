e9a0d2f3316a235ee8b93380fa8a102e81c6cd4c
ludovic ouvry <ludovic.ouvry@univ-lille1.fr>
2019-01-21 02:03:43 +0100

    change create environement

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,8 +7,8 @@ package com.mycompany.app;
 public class App {
 	public static void main(String[] args) {
 		Vue vue = new Vue();
-		//Environement env = new Environement(5, 5, 2,vue);
-		Environement env = new Environement(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]),vue);
+		Environement env = new Environement(5, 5, 2,vue);
+		//Environement env = new Environement(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]),vue);
 		
 		System.out.println(env);
 		for (int i = 0; i < 100; i++) {
