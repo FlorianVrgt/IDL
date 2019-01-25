b1cd62f97a913a727470c5b9440894195d9e5d18
Léane <florian@DESKTOP-KTE7AGF.localdomain>
2019-01-25 01:46:12 +0100

    grille

--- src/main/java/Vue/Jpan.java
+++ src/main/java/Vue/Jpan.java
@@ -1,16 +1,18 @@
 package Vue;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
 
 import javax.swing.JPanel;
 
 import core.Environement;
+import wator.*;
 
 public class Jpan extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	Environement env;
 
 	public Environement getEnv() {
@@ -28,14 +30,32 @@ public class Jpan extends JPanel {
 
 	@Override
 	public void paintComponent(Graphics g) {
-		g.clearRect(0, 0, 1500, 1500);
-		int sizeX=1250/env.getSizeX();
-		int sizeY=1250/env.getSizeY();
+		g.clearRect(0, 0,(int) this.getSize().getWidth(), (int)this.getSize().getHeight());
+		double sizeX=this.getSize().getHeight()/env.getSizeX();
+		double sizeY=this.getSize().getHeight()/env.getSizeY();
+		System.out.println(sizeX + "  et sizeY " + sizeY);
 		if (env != null) {
+			int coorX = 0;
+			int coorY = 0;
+			for(int w = 0; w < env.getSizeX(); w++) {
+				g.drawLine(coorX, 0, coorX, (int) this.getSize().getHeight());
+				coorX += (int) sizeX;
+			}
+			for(int h = 0; h < env.getSizeY(); h++) {
+				g.drawLine(0, coorY, (int) this.getSize().getHeight(), coorY);
+				coorY += (int) sizeY;
+			}
 			for (int i = 0; i < env.getSizeX(); i++) {
 				for (int j = 0; j < env.getSizeY(); j++) {
 					if (env.getGrille()[i][j] != null) {
-						g.fillOval(i*sizeX,j*sizeY, 75, 75);
+						if(env.getGrille()[i][j].getClass() == Fish.class) {
+							g.setColor(new Color(255,0,0));
+							g.fillOval(i*((int)sizeX),j*((int)sizeY), ((int)sizeX), ((int)sizeX));
+						}
+						else {
+							g.setColor(new Color(51,51,51));
+							g.fillOval(i*((int)sizeX),j*((int)sizeY), ((int)sizeX), ((int)sizeX));
+						}
 					} 
 
 				}
--- src/main/java/particules/App.java
+++ src/main/java/particules/App.java
@@ -12,10 +12,11 @@ public class App {
 	public static void main(String[] args) {
 		Vue vue = new Vue();
 		NonTorique env = new NonTorique(10, 10, 5,vue);
+//		Environement env = new Environement(10,10,5,vue);
 		//Environement env = new Environement(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]),vue);
 		
 		System.out.println(env);
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 100; i++) {
 			env.step();
 			
 			System.out.println(env);
